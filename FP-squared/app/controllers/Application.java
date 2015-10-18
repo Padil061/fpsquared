8b488e97c178d99bcbbaf5b2b658bad7f6d22401
Patrick Perey <patrick@Patricks-MacBook-Pro.local>
2015-10-18 16:14:52 -0700

    Removed models commented in our controllers folder

--- FP-squared/app/controllers/Application.java
+++ FP-squared/app/controllers/Application.java
@@ -12,29 +12,3 @@ public class Application extends Controller {
     }
 
 }
-/*
-public private Account {
-
-  private username;
-  private password;
-  private static userId;
-
-}
-
-public class Team {
-    // attributes and methods go in here
-}
-
-public class Sprint extends Controller {
-    // attributes and methods go in here
-}
-
-public class Story extends Controller {
-    // attributes and methods go in here
-}
-
-public class Tasks extends Controller {
-    // attributes and methods go in here
-}
-
-*/
