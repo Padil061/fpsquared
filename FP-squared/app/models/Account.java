21a157edf38aaa5092c4076358ed46fa3ab69096
Pataaar <patrickrp2@gmail.com>
2015-10-28 01:03:47 -0700

    Including some libraries essential for creating models. Refer to them when creating your models

--- FP-squared/app/models/Account.java
+++ FP-squared/app/models/Account.java
@@ -2,6 +2,9 @@ package models;
 
 import com.avaje.ebean.Model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Account extends Model {
     private String userName;
     private String password;
