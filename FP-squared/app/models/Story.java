5cbd1e58130671e0557efe2de6139387e4cdb0f4
Zachary Martin <marti490@cougars.csusm.edu>
2015-11-03 00:41:59 -0800

    Updated imports in story and team models

--- FP-squared/app/models/Story.java
+++ FP-squared/app/models/Story.java
@@ -2,7 +2,10 @@ package models;
 
 import com.avaje.ebean.Model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Story {
--- FP-squared/app/models/Team.java
+++ FP-squared/app/models/Team.java
@@ -2,6 +2,8 @@ package models;
 
 import play.db.ebean.Model;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Team extends Model{
