5f5f1586659556f1b6d7ec6ac2f5de674aa6efb3
sinha002 <sinha002@users.noreply.github.com>
2015-11-02 18:39:06 -0800

    Update Task.java

--- FP-squared/app/models/Task.java
+++ FP-squared/app/models/Task.java
@@ -1,6 +1,35 @@
 package models;
 
-public class Task {
-    private Long id;
-    private String label;
+import play.db.ebean.Model;
+import java.io.*;
+import javax.persistence.Entity;
+
+@Entity
+public class Task{
+
+    String name;
+    Long id;
+    String status;
+
+// This is the constructor of the class Task
+    public Task(String name){
+          this.name = name;
+  }
+
+// Assign the id of the Task to the variable id.     
+     public void taskId(Long taskId){
+             id = taskId;
+   }
+
+// Assign the status of the Task to the variable status.
+       public void taskStatus(String taskStatus){
+                status = taskStatus;
+  }
+
+//Print the Task details.
+        public void printTask(){
+             System.out.println("Name:" + name);
+             System.out.println("Id:" + id);
+             System.out.println("Status:" + status);
+   }
 }
