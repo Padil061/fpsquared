8f469d3cb52de1713fa91e8516024a962bb5dbd9
Zachary Martin <marti490@cougars.csusm.edu>
2015-11-15 21:51:06 -0800

    Added dashboard and welcome stubs

--- FP-squared/app/controllers/Application.java
+++ FP-squared/app/controllers/Application.java
@@ -5,10 +5,20 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.welcome;
+import views.html.dashboard;
 
 public class Application extends Controller {
 
     public Result index() { return ok(index.render()); }
+
+    public Result welcome() {
+        return ok(welcome.render());
+    }
+
+    public Result dashboard(Long id) {
+        return ok(dashboard.render(id));
+    }
 
     public Result createUser() {
         Account account = Form.form(Account.class).bindFromRequest().get();
