5e5523f16f772bdfce41fa9365da615fc927a501
Pataaar <patrickrp2@gmail.com>
2015-11-15 21:34:26 -0800

    Modularized some of our content into seperate scala template files

--- FP-squared/app/controllers/Application.java
+++ FP-squared/app/controllers/Application.java
@@ -8,19 +8,12 @@ import views.html.index;
 
 public class Application extends Controller {
 
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+    public Result index() { return ok(index.render()); }
 
     public Result createUser() {
         Account account = Form.form(Account.class).bindFromRequest().get();
         account.save();
         return redirect(routes.Application.index());
     }
-
-    //public Result getPersons() {
-        //List<Account> accounts = new Model.Finder(String.class, Account.class).all();
-        //return ok(toJson(accounts));
-    //}
 
 }
