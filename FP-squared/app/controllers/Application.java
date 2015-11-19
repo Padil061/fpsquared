cebd9beceaaeae668a098cf6dd11d86fa2c0eae7
Pataaar <patrickrp2@gmail.com>
2015-11-19 13:12:02 -0800

    Adding controllers and new views

--- FP-squared/app/controllers/Application.java
+++ FP-squared/app/controllers/Application.java
@@ -4,26 +4,50 @@ import models.Account;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
-import views.html.welcome;
 import views.html.dashboard;
+import views.html.index;
+import views.html.login;
+import views.html.welcome;
 
 public class Application extends Controller {
 
     public Result index() { return ok(index.render()); }
 
-    public Result welcome() {
-        return ok(welcome.render());
+    public Result login() {
+        Account account = Form.form(Account.class).bindFromRequest().get();
+        //session("connected", account.userName);
+
+        return ok(login.render());
     }
 
-    public Result dashboard(Long id) {
-        return ok(dashboard.render(id));
+    public Result logout() {
+        session().clear();
+        flash("success", "You've logged out successfully");
+        return redirect(routes.Application.login());
+    }
+
+    public Result welcome(String userName) {
+        return ok(welcome.render(userName));
+    }
+
+    public static String authenticateUser() {
+        String user = session("connected");
+        if(user != null) {
+            return "Hello " + user + " you have started your session";
+        } else {
+            return "Oops, you are not connected";
+        }
+    }
+
+    public Result dashboard() {
+        return ok(dashboard.render());
     }
 
     public Result createUser() {
         Account account = Form.form(Account.class).bindFromRequest().get();
         account.save();
-        return redirect(routes.Application.index());
+        session("connected", account.userName);
+        return redirect(routes.Application.welcome(account.userName));
     }
 
 }
